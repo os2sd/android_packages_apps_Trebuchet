5dcb5012a5a234e69645ce335b1793c990019647
burstlam <burstlam@gmail.com>
2012-06-14 21:33:42 +0800

    Turn Off Search Bar by Default

--- src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
+++ src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
@@ -68,7 +68,7 @@ public final class PreferencesProvider {
             }
             public static boolean getShowSearchBar(Context context) {
                 final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
-                return preferences.getBoolean("ui_homescreen_general_search", true);
+                return preferences.getBoolean("ui_homescreen_general_search", false);
             }
             public static boolean getResizeAnyWidget(Context context) {
                 final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
